10edbb89db1905f0eb3395f1f4c84a7824ac5c8c
riya mandal <riyamandal4u@gmail.com>
2024-11-19 20:06:26 +0530

    user details work done

---
+++ lib/widgets/details_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ItemRow extends StatelessWidget {
+  final IconData icon; // Icon for the leading part
+  final String title; // Title text
+  final String text; // Text that follows the colon
+
+  const ItemRow({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(
+            icon,
+            color: Colors.grey.shade700,
+          ),
+          const SizedBox(width: 8),
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.grey.shade800,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Row(
+            children: [
+              const Text(' : '),
+              Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
